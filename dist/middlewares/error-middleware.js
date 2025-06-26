70c7ff425c65ab6dcfcc5f2c4699347399e70f62
Maria Eduarda Jardim <mahjardim02@gmail.com>
2025-06-26 16:42:33 -0300

    fix: summary response

--- dist/middlewares/error-middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = errorHandler;
-function errorHandler(error, req, res, next) {
-    console.error("Erro capturado:", error);
-    const status = error.status || 500;
-    const message = error.message || "Erro interno do servidor";
-    res.status(status).json({ error: message });
-}
--- dist/routers/summary-routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const summary_controller_1 = require("../controllers/summary-controller");
-const summaryRouter = (0, express_1.Router)();
-summaryRouter.get("/summary/:document", summary_controller_1.SummaryByCPF);
-exports.default = summaryRouter;
