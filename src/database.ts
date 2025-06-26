15de12ea294030fcda7b4d057b76cfda1616b1f3
Maria Eduarda Jardim <mahjardim02@gmail.com>
2025-06-26 15:22:23 -0300

    fix: connection with DB SSL

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
-import pg from "pg";
-import dotenv from "dotenv";
+import  pg from "pg";
+import  dotenv from "dotenv";
 
 dotenv.config();
 
 const {Pool} = pg
-const db = new Pool({connectionString: process.env.DATABASE_URL})
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Necessário para o Render
+  },
+});
 
 export default db
